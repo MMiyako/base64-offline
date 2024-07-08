dc9866db098fe09295ab2d17cb4229b4ce9d6980
MMiyako <154000118+MMiyako@users.noreply.github.com>
2024-07-08 22:12:12 +0800

    Add clear & copy functions

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import { Base64 } from "js-base64";
 
 let $decodeBtn = document.getElementById("decode");
 let $encodeBtn = document.getElementById("encode");
+let $clearBtn = document.getElementById("clear");
+let $copyEncodedBtn = document.getElementById("copy-encoded");
+let $copyDecodedBtn = document.getElementById("copy-decoded");
 
 $decodeBtn.addEventListener("click", () => {
     let $encoded = document.getElementById("encoded");
@@ -22,3 +25,40 @@ $encodeBtn.addEventListener("click", () => {
 
     $encoded.value = encoded;
 });
+
+$clearBtn.addEventListener("click", () => {
+    let $encoded = document.getElementById("encoded");
+    let $decoded = document.getElementById("decoded");
+
+    $encoded.value = "";
+    $decoded.value = "";
+    $encoded.focus();
+});
+
+$copyEncodedBtn.addEventListener("click", function () {
+    let encoded = document.getElementById("encoded").value;
+    copy(this, encoded);
+});
+
+$copyDecodedBtn.addEventListener("click", function () {
+    let decoded = document.getElementById("decoded").value;
+    copy(this, decoded);
+});
+
+function copy(button, value) {
+    if (button.dataset.timeoutId) {
+        clearTimeout(button.dataset.timeoutId);
+    }
+
+    button.childNodes[0].nodeValue = "Copied";
+    button.classList.add("copied");
+    navigator.clipboard.writeText(value);
+
+    let timeoutId = setTimeout(() => {
+        button.childNodes[0].nodeValue = "Copy";
+        button.classList.remove("copied");
+        button.dataset.timeoutId = "";
+    }, 1000);
+
+    button.dataset.timeoutId = timeoutId;
+}
